00e36f2fd946d78215b50a4a018a93c73ddc331a
Nigel Lee <lee.nigel@hotmail.com>
2020-03-14 16:49:03 +0800

    added swagger spec

---
+++ config/swaggerConfig.js
@@ -0,0 +1,22 @@
+const swaggerConfig = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'CZ2006 API',
+      version: '1.0.0',
+      description: 'APIs for CZ2006',
+      license: {
+        name: 'MIT',
+        url: 'https://choosealicense.com/licenses/mit/'
+      }
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1'
+      }
+    ]
+  },
+  apis: ['./controllers/sample.js']
+}
+
+module.exports = swaggerConfig
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
+const swaggerConfig = require('./config/swaggerConfig')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv')
 
 if (process.env.NODE_ENV != 'production') dotenv.config()
 
-connectDB()
+// connectDB()
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const swaggerSpec = swaggerJSDoc(swaggerConfig)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/docs', swaggerUI.serve)
+app.get('/docs', swaggerUI.setup(swaggerSpec, { explorer: true }))
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to CZ2006 COBLIMA</h1>')
