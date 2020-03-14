6b0a199a5fc39a826a98575b3f3bc2d16e3c05eb
Nigel Lee <lee.nigel@hotmail.com>
2020-03-14 11:32:46 +0800

    setup linting and husky

--- config/db.js
+++ config/db.js
@@ -1,26 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  let connectionString;
-  if (process.env.NODE_ENV == "production") {
+  let connectionString
+  if (process.env.NODE_ENV == 'production') {
     connectionString =
-      "mongodb://" +
+      'mongodb://' +
       process.env.MONGO_USER +
-      ":" +
+      ':' +
       process.env.MONGO_PW +
-      "@mongo:27017/prod?retryWrites=true&w=majority";
+      '@mongo:27017/prod?retryWrites=true&w=majority'
   } else {
     connectionString =
-      "mongodb+srv://mongo:" +
+      'mongodb+srv://mongo:' +
       process.env.MONGO_PW +
-      "@cluster0-lnewo.gcp.mongodb.net/test?retryWrites=true&w=majority";
+      '@cluster0-lnewo.gcp.mongodb.net/test?retryWrites=true&w=majority'
   }
 
-  console.log(connectionString);
+  console.log(connectionString)
   await mongoose.connect(connectionString, (error, db) => {
-    if (error) console.log(error);
-    else console.log("Connected to MongoDB!");
-  });
-};
+    if (error) console.log(error)
+    else console.log('Connected to MongoDB!')
+  })
+}
 
-module.exports = connectDB;
+module.exports = connectDB
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const dotenv = require("dotenv");
+const express = require('express')
+const connectDB = require('./config/db')
+const dotenv = require('dotenv')
 
-if (process.env.NODE_ENV != "production") dotenv.config();
+if (process.env.NODE_ENV != 'production') dotenv.config()
 
-connectDB();
-const app = express();
+connectDB()
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to CZ2006 COBLIMA</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to CZ2006 COBLIMA</h1>')
+})
 
 app.listen(5000, () => {
-  console.log("App running on port 5000");
-});
+  console.log('App running on port 5000')
+})
